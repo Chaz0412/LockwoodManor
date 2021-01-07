f3c93b98577a4588f5f4e0959849b3df956bd830
Charlie Carroll <55137243+Chaz0412@users.noreply.github.com>
2021-01-07 18:45:24 +0000

    Map Updates

---
+++ LockwoodManorGameEngine2/Assets/Scripts/DoorInteract.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorInteract : Interactable
+{
+    public bool isOpen;
+
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isOpen = animator.GetBool("Open");
+        
+    }
+
+    // Update is called once per frame
+    void UpdateDoor()
+    {
+        
+        if(isOpen == true)
+        {
+            animator.SetBool("Open", false);
+            isOpen = false;
+        }
+        else
+        {
+            animator.SetBool("Open", true);
+            isOpen = true;
+        }
+        
+    }
+
+    public override string GetDescription()
+    {
+        if (isOpen) return "Press [E] to <color=red>close</color> the door.";
+        return "Press [E] to <color=green>open</color> the door.";
+    }
+
+    public override void Interact()
+    {
+        UpdateDoor();
+    }
+}
